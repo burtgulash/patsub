ddb1129f13bd87d86cd90671fe71811d28ef91e7
Io <me@me.me>
2021-02-16 20:24:24 +0100

    yo

--- patsub/src/main.rs
+++ patsub/src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
-//use std::io::{self,BufRead,Write};
+use std::io::{self,BufRead,Write};
 
-//extern crate regex;
-//use regex::Regex;
+extern crate regex;
+use regex::Regex;
 
 #[derive(Debug)]
 enum Parsed {
-    Base(String),
+    Str(String),
     Tree(Vec<Parsed>),
 }
 
@@ -16,13 +16,13 @@ fn parse(s: &Vec<char>, mut i: usize, lvl: usize) -> (usize, Parsed) {
 
     while i < s.len() {
         if s[i] == '{' {
-            tree.push(Parsed::Base(buf.iter().collect()));
-            let (j, subtree) = parse(s, i + 1, lvl + 1);
-            i = j;
+            tree.push(Parsed::Str(buf.iter().collect()));
+            let (i_, subtree) = parse(s, i + 1, lvl + 1);
+            i = i_;
             tree.push(subtree);
             buf.clear();
         } else if s[i] == '}' {
-            tree.push(Parsed::Base(buf.iter().collect()));
+            tree.push(Parsed::Str(buf.iter().collect()));
             if lvl == 0 && i < s.len() - 1 {
                 //raise ValueError("Parse error: closing }")
             }
@@ -39,7 +39,7 @@ fn parse(s: &Vec<char>, mut i: usize, lvl: usize) -> (usize, Parsed) {
 
 fn to_regex_(tree: &[Parsed], is_group: bool, result: &mut Vec<String>) {
     if is_group {
-        let name = match tree[0] {Parsed::Base(ref s) => s, _ => {panic!("unreachable")} };
+        let name = match tree[0] {Parsed::Str(ref s) => s, _ => {panic!("unreachable")} };
         let rx = "[^/]*";
         result.push(format!("(?P<{}>{}", name, rx));
 
@@ -48,7 +48,7 @@ fn to_regex_(tree: &[Parsed], is_group: bool, result: &mut Vec<String>) {
     } else {
         for x in tree {
             match x {
-                Parsed::Base(s) => result.push(s.to_string()),
+                Parsed::Str(s) => result.push(s.to_string()),
                 Parsed::Tree(ref subtree) => to_regex_(subtree, true, result),
             }
         }
@@ -66,38 +66,70 @@ fn to_regex(tree: &Parsed) -> String {
     result.join("")
 }
 
+fn assemble(s: &[String]) -> String {
+    let mut res: Vec<char> = s[0].chars().collect();
+    for i in (1..s.len()).step_by(2) {
+        res.push('<');
+        res.extend(s[i].chars());
+        res.push('>');
+        res.extend(s[i + 1].chars());
+    }
+    res.iter().collect()
+}
+
+fn parse_sub(s: &str) -> Vec<String> {
+    let mut res = Vec::new();
+    for part in s.split(&['{', '}'][..]) {
+        res.push(String::from(part));
+    }
+    res
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("argz {:?}", args);
 
-    let pat = format!("{{{}}}", &args[1]);
-    let (_, tree) = parse(&pat.chars().collect(), 0, 0);
-    let rx = to_regex(&tree);
-    println!("rx {}", rx);
-//
-//    let mut pats_: Vec<&str> = Vec::new();
-//    let mut subs_: Vec<&str> = Vec::new();
-//
-//    for i in (1..args.len()).step_by(2) {
-//        pats_.push(args[i].as_ref());
-//        if i + 1 == args.len() {
-//            subs_.push(args[i].as_ref());
-//        } else {
-//            subs_.push(args[i + 1].as_ref());
-//        }
-//    }
-//    println!("{:?}", pats_);
-//    println!("{:?}", subs_);
-//    let res: Vec<Regex> = args.iter().map(|x| Regex::new(x.as_str()).unwrap()).collect();
-//
-//    for line in io::stdin().lock().lines() {
-//        let s = line.unwrap();
-//        for (re, _) in res.iter().by_ref().zip(res.iter().by_ref()) {
-//            if let Some(m) = re.find(s.as_str()) {
-//                let _ = io::stdout().write_all(s.as_bytes());
-//                let _ = io::stdout().write_all(b"\n");
-//                let _ = io::stdout().flush();
-//            }
-//        }
-//    }
+//    let pat = format!("{{{}}}", &args[1]);
+//    let (_, tree) = parse(&pat.chars().collect(), 0, 0);
+//    let rx = to_regex(&tree);
+//    println!("rx {}", rx);
+
+    let mut pats_: Vec<&str> = Vec::new();
+    let mut subs_: Vec<&str> = Vec::new();
+
+    for i in (1..args.len()).step_by(2) {
+        pats_.push(args[i].as_ref());
+        if i + 1 == args.len() {
+            subs_.push(args[i].as_ref());
+        } else {
+            subs_.push(args[i + 1].as_ref());
+        }
+    }
+    println!("{:?}", pats_);
+    println!("{:?}", subs_);
+
+    let pats: Vec<Regex> = pats_.iter().map(|arg| {
+        let pat = format!("{{{}}}", arg);
+        let (_, tree) = parse(&pat.chars().collect(), 0, 0);
+        let rx = to_regex(&tree);
+        //println!("rx {}", rx);
+
+        Regex::new(&rx).unwrap()
+    }).collect();
+
+    let subs: Vec<Vec<String>> = subs_.iter().map(|x| parse_sub(x)).collect();
+    println!("DEBUG {:?}", subs);
+
+    for line in io::stdin().lock().lines() {
+        let x = line.unwrap();
+        for (pat, sub) in pats.iter().by_ref().zip(subs.iter()) {
+            //println!("PAT {:?}", pat);
+            if let Some(m) = pat.find(x.as_str()) {
+                let y = assemble(&sub[..]);
+                let _ = io::stdout().write_all(y.as_bytes());
+                let _ = io::stdout().write_all(b"\n");
+                let _ = io::stdout().flush();
+            }
+        }
+    }
 }
